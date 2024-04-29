15a4f7ed13bd2de84e8e76e77fb8d6de32a31513
entaromia <entaromia@gmail.com>
2024-04-30 00:27:27 +0300

    GUI: Fix string to int conversion logic
    The app would crash on invalid input in the previous implementation
    
    WPF does handle this by itself, so this was overlooked

--- LabelGenGUI.Avalonia/Converters/EmptyToIntConverter.cs
+++ LabelGenGUI.Avalonia/Converters/EmptyToIntConverter.cs
@@ -5,14 +5,14 @@ using System.Globalization;
 namespace LabelGenGUI.Avalonia.Converters;
 
 /// <summary>
-/// Converts empty string inputs to zero in order to avoid null integers
+/// Converts empty or invalid string inputs to zero
 /// </summary>
 public class EmptyToIntConverter : IValueConverter
 {
     public object? Convert(object? value, Type? targetType, object? parameter, CultureInfo culture)
     => value?.ToString();
 
-    // replace empty string input with '0'
+    // replace empty or invalid input with '0'
     public object? ConvertBack(object? value, Type? targetType, object? parameter, CultureInfo culture)
-    => value is string input && string.IsNullOrEmpty(input) ? 0 : System.Convert.ToInt32(value);
+        => value is string input && int.TryParse(input, out int result) ? result : 0;
 }
