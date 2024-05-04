bfa577e37612539effde590c5e6c93def6cd0813
entaromia <entaromia@gmail.com>
2024-05-04 15:00:09 +0300

    GUI: Switch to NumericUpDown for count input
    Hide arrow keys as our input fields are too small for that
    Convert null values to zero

---
+++ LabelGenGUI.Avalonia/Converters/EmptyToZeroConverter.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace LabelGenGUI.Avalonia.Converters
+{
+    /// <summary>
+    /// Converts empty inputs to zero
+    /// </summary>
+    public class EmptyToZeroConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is null ? 0M : System.Convert.ToDecimal(value);
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is null ? 0 : System.Convert.ToInt32(value);
+    }
+}
