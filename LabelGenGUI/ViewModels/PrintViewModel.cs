7e9fa84a6b954bb17278872a4b95a181cccfdfa7
entaromia <entaromia@gmail.com>
2024-06-29 00:23:43 +0300

    GUI: Go back from print ui

--- LabelGenGUI/ViewModels/PrintViewModel.cs
+++ LabelGenGUI/ViewModels/PrintViewModel.cs
@@ -74,5 +74,10 @@ namespace LabelGenGUI.ViewModels
         {
             Dispatcher.UIThread.Post(() => { ProgressMessage = message; });
         }
+
+        public void GoBack()
+        {
+            NavigationService.Instance.Navigate(NavigationService.Pages.ShoeListPage);
+        }
     }
 }
