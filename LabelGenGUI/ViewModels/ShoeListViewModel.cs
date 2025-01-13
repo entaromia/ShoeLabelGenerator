b3e7c1de5469fabbd560a326d09360b51c97d3ca
entaromia <entaromia@gmail.com>
2025-01-13 19:09:18 +0300

    GUI: Allow printing selection
     * Only printing the whole list was supported until now

--- LabelGenGUI/ViewModels/ShoeListViewModel.cs
+++ LabelGenGUI/ViewModels/ShoeListViewModel.cs
@@ -1,25 +1,47 @@
-﻿using LabelGenGUI.Services;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using LabelGenGUI.Services;
 using ShoeLabelGen.Common;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace LabelGenGUI.ViewModels
 {
-    public class ShoeListViewModel() : ViewModelBase
+    public partial class ShoeListViewModel() : ViewModelBase
     {
         public static ObservableCollection<ShoeListItem> ListItems => ShoeListService.Instance.GetItems();
 
+        [ObservableProperty]
+        private int selectedIndex;
+
         /// <summary>
-        /// Do not allow deleting if no item is selected
+        /// Do not allow operations if no item is selected
         /// </summary>
-        public bool CanDeleteItem(object msg) => (int)msg >= 0;
-        public void DeleteItem(object msg)
+        public bool CanOperateOnItem() => SelectedIndex >= 0;
+
+        [RelayCommand(CanExecute = nameof(CanOperateOnItem))]
+        public void DeleteItem()
         {
-            ShoeListService.Instance.DeleteItem((int)msg);
+            ShoeListService.Instance.DeleteItem(SelectedIndex);
         }
 
         public void AddItem()
         {
             NavigationService.Instance.Navigate(NavigationService.Pages.InputPage);
+        }
+
+        [RelayCommand(CanExecute = nameof(CanOperateOnItem))]
+        public async Task PrintBoxItem()
+        {
+            string zplText = PrintService.Instance.BoxesToZpl([ListItems[SelectedIndex]]);
+            await PrintService.Instance.PrintZpl(zplText);
+        }
+
+        [RelayCommand(CanExecute = nameof(CanOperateOnItem))]
+        public async Task PrintParcelItem(object msg)
+        {
+            string zplText = PrintService.Instance.ParcelsToZpl([ListItems[SelectedIndex]]);
+            await PrintService.Instance.PrintZpl(zplText);
         }
     }
 }
